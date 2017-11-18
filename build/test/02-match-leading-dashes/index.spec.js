cf9013856ba79846a5a2892aefb33406177003ba
James-Alex Matthews <vamtiger@vamtiger.com>
2017-11-18 10:45:26 +0200

    Latest Build

--- build/test/02-match-leading-dashes/index.spec.js
+++ build/test/02-match-leading-dashes/index.spec.js
@@ -10,15 +10,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const chai_1 = require("chai");
 const XRegExp = require("xregexp");
-const regex = require("../../index");
+const index_1 = require("../../index");
 describe("vamtiger-regex-leading-dash: regex.default", function () {
-    it("should match a leading dash", function () {
+    it("should match a leading dashes", function () {
         return __awaiter(this, void 0, void 0, function* () {
-            const input = '-leadingDash', match = XRegExp.exec(input, regex.default);
+            const input = '--leadingDashes', match = input.match(index_1.default), xMatch = XRegExp.exec(input, index_1.default);
             chai_1.expect(match).to.be.ok;
-            chai_1.expect(match.dash).to.be.ok;
-            chai_1.expect(match.dash).to.equal('-');
-            chai_1.expect(match.afterDash).to.equal('leadingDash');
+            chai_1.expect(match.length).to.be.gt(0);
+            chai_1.expect(match[0]).to.equal(input);
+            chai_1.expect(match[1]).to.equal('--');
+            chai_1.expect(match[2]).to.equal('leadingDashes');
+            chai_1.expect(xMatch).to.be.ok;
+            chai_1.expect(xMatch.dash).to.be.ok;
+            chai_1.expect(xMatch.dash).to.equal('--');
+            chai_1.expect(xMatch.afterDash).to.equal('leadingDashes');
         });
     });
 });
--- source/test/02-match-leading-dashes/index.spec.ts
+++ source/test/02-match-leading-dashes/index.spec.ts
@@ -3,22 +3,22 @@ import * as XRegExp from 'xregexp';
 import {default as regex, Match} from '../../index';
 
 describe("vamtiger-regex-leading-dash: regex.default", function () {
-    it("should match a leading dash", async function () {
-        const input = '-leadingDash',
-            match = input.match(regex) as RegExpExecArray,
+    it("should match a leading dashes", async function () {
+        const input = '--leadingDashes',
+            match = input.match(regex) as RegExpMatchArray,
             xMatch: Match = XRegExp.exec(input, regex);
 
         expect(match).to.be.ok;
         expect(match.length).to.be.gt(0);
 
         expect(match[0]).to.equal(input);
-        expect(match[1]).to.equal('-');
-        expect(match[2]).to.equal('leadingDash');
+        expect(match[1]).to.equal('--');
+        expect(match[2]).to.equal('leadingDashes');
 
         expect(xMatch).to.be.ok;
         expect(xMatch.dash).to.be.ok;
 
-        expect(xMatch.dash).to.equal('-');
-        expect(xMatch.afterDash).to.equal('leadingDash');
+        expect(xMatch.dash).to.equal('--');
+        expect(xMatch.afterDash).to.equal('leadingDashes');
     })
 })
